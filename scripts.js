cb7cbaad01a10444eb259aee1a4d660d388bcea2
Nick Barnes <Nicholas.Barnes4194@gmail.com>
2021-05-12 09:45:42 -0500

    works the way i want it to but not pretty

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 const baseURL = "https://pokeapi.co/api/v2/pokemon/";
+const picUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' 
 let url;
+let pokeUrl;
 
 const searchNum = document.querySelector('.search');
 const submitBtn = document.querySelector('.submit');
 const submitSearch = document.querySelector('form');
 const img = document.querySelector('img')
+const pokemonName = document.querySelector('.name');
 submitSearch.addEventListener('submit', fetchPokemon)
 
 
@@ -14,6 +17,7 @@ submitSearch.addEventListener('submit', fetchPokemon)
 function fetchPokemon(e){
     e.preventDefault();
     url = baseURL + searchNum.value;
+    pokeUrl = picUrl +searchNum.value;
     fetch(baseURL)
     .then((results) =>{
         return results.json();
@@ -22,36 +26,37 @@ function fetchPokemon(e){
         console.log(submission);
         console.log(searchNum.value);
         console.log(url);
-        fetchPicture();
+        console.log(pokeUrl);
+        pokeName = submission.results[searchNum.value - 1].name;
+        console.log(pokeName);
+        displayPoke();
+        displayName();
+        // fetchPicture();
     })
     .catch(err => console.log(err))
 }
 
-
-function fetchPicture(){
-fetch(`${url}`)
-    .then(results=>{
-        return results.json();
-    })
-        .then(submission =>{
-            console.log(submission)
-            displayPoke(submission);
-            console.log(url);
-        })
-}
+// function fetchPicture(){
+// fetch(`${url}`)
+//     .then(results=>{
+//         return results.json();
+//     })
+//         .then(submissionTwo =>{
+//             console.log(submissionTwo)
+//             displayName();
+//             console.log(url);
+//             pokemon = submissionTwo.results
+//             console.log(pokemon);
+//             console.log(submissionTwo.sprites.other.official-artwork.front_default);
+//         })
+// }
 
 function displayPoke(){
-    let pokeFriend = `${url}.sprites.other.official-artwork.front_default.url`;
+    let pokeFriend = `${pokeUrl}.png`;
     console.log(pokeFriend);
-    // let tinyPokeFried = url.response.sprites.front_default;
-    // let img = document.querySelector('image');
     img.src = pokeFriend;
-    // if(pokeFriend.length === 1){
-        // nav.style.display = 'block';
-        
-    // } else {
-        // nav.style.display = 'block;'
-        // img.src = tinyPokeFried;
+   }
 
-    }
-// }
+   function displayName(){
+    pokemonName.innerText = pokeName;
+   }
